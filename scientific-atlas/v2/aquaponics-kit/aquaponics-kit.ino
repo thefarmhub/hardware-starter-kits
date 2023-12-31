c7bc4fd2a983a08b2c71bcd169991b54c4cd2e8c
Jonathan Reyes <me@jonathanreyes.com>
2023-12-31 16:08:13 -0500

    fix: added go templating variables

--- scientific-atlas/v2/aquaponics-kit/aquaponics-kit.ino
+++ scientific-atlas/v2/aquaponics-kit/aquaponics-kit.ino
@@ -8,46 +8,29 @@
 #include <PubSubClient.h> // https://github.com/knolleary/pubsubclient
 
 // ------------ Start Configuration ------------ //
-const char *WIFI_SSID = "";
-const char *WIFI_PASSWORD = "";
+const char *WIFI_SSID = "{{- .WiFiSSID -}}";
+const char *WIFI_PASSWORD = "{{- .WiFiPassword -}}";
 
 // You can get the following variables from the
 // FarmHub dashboard under the "Sensors" tab
-const char THINGNAME[] = "";
-const char TOPIC_PH[] = "";
-const char TOPIC_EC[] = "";
-const char TOPIC_DO[] = "";
-const char TOPIC_TEMP[] = "";
-const char TOPIC_HUM[] = "";
-const char TOPIC_CO2[] = "";
+const char THINGNAME[] = "{{- .ThingName -}}";
+const char TOPIC_PH[] = "{{- .TopicPH -}}";
+const char TOPIC_EC[] = "{{- .TopicEC -}}";
+const char TOPIC_DO[] = "{{- .TopicDO -}}";
+const char TOPIC_TEMP[] = "{{- .TopicTEMP -}}";
+const char TOPIC_HUM[] = "{{- .TopicHUM -}}";
+const char TOPIC_CO2[] = "{{- .TopicCO2 -}}";
 
 static const char FARMHUB_CERT_CRT[] PROGMEM = R"KEY(
+{{- .CertificatePEM -}}
 )KEY";
 
 static const char FARMHUB_CERT_PRIVATE[] PROGMEM = R"KEY(
+{{- .CertificatePrivateKey -}}
 )KEY";
 
 static const char FARMHUB_CERT_CA[] PROGMEM = R"EOF(
------BEGIN CERTIFICATE-----
-MIIDQTCCAimgAwIBAgITBmyfz5m/jAo54vB4ikPmljZbyjANBgkqhkiG9w0BAQsF
-ADA5MQswCQYDVQQGEwJVUzEPMA0GA1UEChMGQW1hem9uMRkwFwYDVQQDExBBbWF6
-b24gUm9vdCBDQSAxMB4XDTE1MDUyNjAwMDAwMFoXDTM4MDExNzAwMDAwMFowOTEL
-MAkGA1UEBhMCVVMxDzANBgNVBAoTBkFtYXpvbjEZMBcGA1UEAxMQQW1hem9uIFJv
-b3QgQ0EgMTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALJ4gHHKeNXj
-ca9HgFB0fW7Y14h29Jlo91ghYPl0hAEvrAIthtOgQ3pOsqTQNroBvo3bSMgHFzZM
-9O6II8c+6zf1tRn4SWiw3te5djgdYZ6k/oI2peVKVuRF4fn9tBb6dNqcmzU5L/qw
-IFAGbHrQgLKm+a/sRxmPUDgH3KKHOVj4utWp+UhnMJbulHheb4mjUcAwhmahRWa6
-VOujw5H5SNz/0egwLX0tdHA114gk957EWW67c4cX8jJGKLhD+rcdqsq08p8kDi1L
-93FcXmn/6pUCyziKrlA4b9v7LWIbxcceVOF34GfID5yHI9Y/QCB/IIDEgEw+OyQm
-jgSubJrIqg0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMC
-AYYwHQYDVR0OBBYEFIQYzIU07LwMlJQuCFmcx7IQTgoIMA0GCSqGSIb3DQEBCwUA
-A4IBAQCY8jdaQZChGsV2USggNiMOruYou6r4lK5IpDB/G/wkjUu0yKGX9rbxenDI
-U5PMCCjjmCXPI6T53iHTfIUJrU6adTrCC2qJeHZERxhlbI1Bjjt/msv0tadQ1wUs
-N+gDS63pYaACbvXy8MWy7Vu33PqUXHeeE6V/Uq2V8viTO96LXFvKWlJbYK8U90vv
-o/ufQJVtMVT8QtPHRh8jrdkPSHCa2XV4cdFyQzR1bldZwgJcJmApzyMZFo6IQ6XU
-5MsI+yMRQ+hDKXJioaldXgjUkK642M4UwtBV8ob2xJNDd2ZhwLnoQdeXeGADbkpy
-rqXRfboQnoZsG4q5WTP468SQvvG5
------END CERTIFICATE-----
+{{- .RootCertificateAuthority -}}
 )EOF";
 
 const char FARMHUB_IOT_ENDPOINT[] = "iot.farmhub.ag";
